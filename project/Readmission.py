38fb04394578aa23ca00269c3712b953f1643dc4
H H Arvind <arvindhh99@gmail.com>
2022-08-01 15:48:32 +0530

    Update Readmission.py

--- project/Readmission.py
+++ project/Readmission.py
@@ -4,28 +4,6 @@ from PIL import Image
 import base64
 
 st.set_page_config(page_title='Hospital Readmission',page_icon="🚑")
-
-def get_base64(bin_file):
-    with open(bin_file, 'rb') as f:
-        data = f.read()
-    return base64.b64encode(data).decode()
-
-def set_background(png_file):
-    bin_str = get_base64(png_file)
-    page_bg_img = '''
-    <style>
-    .stApp {
-    background-image: url("data:image/png;base64,%s");
-    background-size: cover;
-    background-repeat: no-repeat;
-    background-position: right center;
-    background-size: 200px 200px;
-    }
-    </style>
-    ''' % bin_str
-    st.markdown(page_bg_img, unsafe_allow_html=True)
-set_background("./project/istockphoto-1307873015-612x612.jpg")
-
 st.header("Predict the hospital Re-admission")
 
 selected = option_menu(menu_title=None,options=["Prediction","Project","Information"],
@@ -44,6 +22,28 @@ if selected == "Prediction":
     if confirm:
         st.success(f"Your age is {Age}")
 
+if selected == "Information":
+    st.markdown("### Information about Diabetes")
+    def get_base64(bin_file):
+    with open(bin_file, 'rb') as f:
+        data = f.read()
+    return base64.b64encode(data).decode()
+
+    def set_background(png_file):
+        bin_str = get_base64(png_file)
+        page_bg_img = '''
+        <style>
+        .stApp {
+        background-image: url("data:image/png;base64,%s");
+        background-size: cover;
+        background-repeat: no-repeat;
+        background-position: right center;
+        background-size: 200px 200px;
+        }
+        </style>
+        ''' % bin_str
+        st.markdown(page_bg_img, unsafe_allow_html=True)
+    set_background("./project/istockphoto-1307873015-612x612.jpg")
     
 
 
