244e8f7670b4522e339a87ebb1a03a1bb3d2cbc3
H H Arvind <arvindhh99@gmail.com>
2022-08-01 15:38:10 +0530

    Update Readmission.py

--- project/Readmission.py
+++ project/Readmission.py
@@ -4,6 +4,25 @@ from PIL import Image
 import base64
 
 st.set_page_config(page_title='Hospital Readmission',page_icon="🚑")
+
+def get_base64(bin_file):
+    with open(bin_file, 'rb') as f:
+        data = f.read()
+    return base64.b64encode(data).decode()
+
+def set_background(png_file):
+    bin_str = get_base64(png_file)
+    page_bg_img = '''
+    <style>
+    .stApp {
+    background-image: url("data:image/png;base64,%s");
+    background-size: cover;
+    }
+    </style>
+    ''' % bin_str
+    st.markdown(page_bg_img, unsafe_allow_html=True)
+set_background("/project/health.png")
+
 st.header("Predict the hospital Re-admission")
 
 selected = option_menu(menu_title=None,options=["Prediction","Project","Information"],
